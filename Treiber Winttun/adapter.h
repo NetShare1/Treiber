e9a5b49058c322dfc59696cca1b599e697eb2d62
Stefan Streimel <stefan@streimel.com>
2020-11-17 16:37:50 +0100

    Es ist jetzt möglich die IP Addresse vom Adapter zu ändern.

--- Treiber Winttun/adapter.h
+++ Treiber Winttun/adapter.h
@@ -1,9 +1,13 @@
 #ifndef ADAPTER
 #define ADAPTER
 
+#include <netioapi.h>
+
 #include "wintun.h"
 #include "log.h"
 
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
 
 // Gets the Handle of the requested Adapter if the adapter does not exist yet
 // the adapter will be created and the handle will be returned after
@@ -35,7 +39,8 @@ WINTUN_ADAPTER_HANDLE getAdapterHandle(
         );
 
         if (adapterHandle == NULL) {
-            return LogLastError(L"Failed to create adapter Handle");
+            LogLastError(L"Failed to create adapter Handle");
+            return NULL;
         }
 
         Log(WINTUN_LOG_INFO, L"Sucessfully create Adapter");
@@ -46,5 +51,28 @@ WINTUN_ADAPTER_HANDLE getAdapterHandle(
     return adapterHandle;
 }
 
+// Sets the Ip address on the adapter give over the LUID
+void setIPAddress(
+    WINTUN_ADAPTER_HANDLE adapter,
+    UINT8 subnetbits, // how many bits are the subnet
+    int ipp1, // first number of ip address
+    int ipp2, // second number of ip address
+    int ipp3, // third number of ip address
+    int ipp4  // fourth number of ip address
+) {
+    MIB_UNICASTIPADDRESS_ROW AddressRow;
+    InitializeUnicastIpAddressEntry(&AddressRow);
+    WintunGetAdapterLUID(adapter, &AddressRow.InterfaceLuid);
+    AddressRow.Address.Ipv4.sin_family = AF_INET;
+    AddressRow.Address.Ipv4.sin_addr.S_un.S_addr = htonl((ipp1 << 24) | (ipp2 << 16) | (ipp3 << 8) | (ipp4 << 0)); /* 10.6.7.7 */
+    AddressRow.OnLinkPrefixLength = subnetbits; /* This is a /24 network */
+    DWORD LastError = CreateUnicastIpAddressEntry(&AddressRow);
+    if (LastError != ERROR_SUCCESS && LastError != ERROR_OBJECT_ALREADY_EXISTS)
+    {
+        LogError(L"Failed to set IP address", LastError);
+    }
+
+    Log(WINTUN_LOG_INFO, L"Set Ip Adress to: %i.%i.%i.%i/%i", ipp1, ipp2, ipp3, ipp4, subnetbits);
+}
 
 #endif // Adapter
--- Treiber Winttun/main.c
+++ Treiber Winttun/main.c
@@ -1,3 +1,5 @@
+#include <Winsock2.h>
+
 #include "init_wintun.h"
 #include "wintun.h"
 #include "log.h"
@@ -9,11 +11,19 @@ int main() {
     HMODULE Wintun = InitializeWintun();
     if (!Wintun)
         return LogError(L"Failed to initialize Wintun", GetLastError());
+
+    WintunSetLogger(ConsoleLogger);
         
     Log(WINTUN_LOG_INFO, L"Sucessfully loaded wintun dll");
 
+    DWORD Version = WintunGetRunningDriverVersion();
+    Log(WINTUN_LOG_INFO, L"Wintun v%u.%u loaded", (Version >> 16) & 0xff, (Version >> 0) & 0xff);
+
     WINTUN_ADAPTER_HANDLE adapterHandle = 
         getAdapterHandle(L"Netshare", L"Netshare ADP1");
+
+    // set ip address of  Adapter
+    setIPAddress(adapterHandle, 24, 10, 0, 0, 3);
 
     WINTUN_SESSION_HANDLE sessionHandle = 
         WintunStartSession(
